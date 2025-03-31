633e05a7eb27fd6a2b9f648e77d4a4b5f6645828
vivek0902 <viveksharma090297@gmail.com>
2025-03-31 18:26:52 +0530

    update package.json vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "scroll-top-bottom-specific-sections-scroll-indicator-in-react",
+  base: "scroll-top-bottom-specific-sections-indicator",
   plugins: [react()],
 });
