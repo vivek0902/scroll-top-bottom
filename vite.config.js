27234f5f98cb69d4f43b8fed1b80f260931d527a
vivek0902 <viveksharma090297@gmail.com>
2025-03-31 18:32:47 +0530

    update bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "scroll-top-bottom-specific-sections-indicator",
+  base: "scroll-top-bottom",
   plugins: [react()],
 });
